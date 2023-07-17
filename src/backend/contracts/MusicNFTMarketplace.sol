504dbe0b4b5dbfe8ad1a6f64136d00e503680537
ayush2905 <ayushagrawal2905@gmail.com>
2023-07-17 23:13:15 +0530

    some changes

--- src/backend/contracts/MusicNFTMarketplace.sol
+++ src/backend/contracts/MusicNFTMarketplace.sol
@@ -3,3 +3,9 @@ pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract MusicNFTMarketplace is ERC721, Ownable {
+    //points to data where all the music data for nfts is located on IPFS
+    string public baseURI = "https://bafybeidhjjbjonyqcahuzlpt7sznmh4xrlbspa3gstop5o47l6gsiaffee.ipfs.nftstorage.link/";
+    
+}
