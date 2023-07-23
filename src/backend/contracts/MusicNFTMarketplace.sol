1d7fc311048bb03686856dae81375948e9ee7b39
ayush2905 <ayushagrawal2905@gmail.com>
2023-07-23 06:43:45 +0530

    initializations done

--- src/backend/contracts/MusicNFTMarketplace.sol
+++ src/backend/contracts/MusicNFTMarketplace.sol
@@ -9,4 +9,33 @@ contract MusicNFTMarketplace is ERC721("ShrodyBuck", "Shrody"), Ownable {
     //points to data where all the music data for nfts is located on IPFS
     string public baseURI =
         "https://bafybeidhjjbjonyqcahuzlpt7sznmh4xrlbspa3gstop5o47l6gsiaffee.ipfs.nftstorage.link/"; //nft uploaded on ipfs
+    string public baseExtension = ".json"; //of metadata
+    address public artist; //etheruem address
+    uint256 public royaltyFee;
+
+    struct MarketItem {
+        uint256 tokenId;
+        address payable seller;
+        uint256 price;
+    }
+    MarketItem[] public marketItems;
+
+    //the deployer will be the initial seller of each nft and will have to pay royalty fee that is why it is marked payable
+    constructor(
+        uint256 _royaltyFee,
+        address _artist,
+        uint256[] memory _prices
+    ) payable {
+        require(
+            _prices.length * _royaltyFee <= msg.value,
+            "Deployer must pay royalty fee for each token listed on the marketplace"
+        );
+        royaltyFee = _royaltyFee;
+        artist = _artist;
+        for (uint8 i = 0; i < _prices.length; i++) {
+            require(_prices[i] > 0, " Price must be greater than zero");
+            _mint(address(this), i); //inherited from ERC721, address of the contract
+            marketItems.push(MarketItem(i, payable(msg.sender), _prices[i])); //msg.sender -> deployer here
+        }
+    }
 }
